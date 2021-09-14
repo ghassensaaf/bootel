4f802fe8d9317d7c5915c26ec90d57fa853e0399
Ghassen Saaf <ghassen.saaf@esprit.tn>
2021-09-14 12:57:35 +0100

    template step2

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {  
+router.post('/', function(req, res, next) {  
+    test =[]; 
+    if(typeof(req.body.pensionId)==="string"){
+        test=JSON.parse(req.body.pensionId);
+    }
+    else{
+        for(let i=0;i<req.body.pensionId.length;i++){
+            test.push(JSON.parse(req.body.pensionId[i]));
+        }
+    }
+    
+    console.log(test);
     res.render('checkout', { title: 'Bootel | checkout',layout :'layouts/main'});
 });
 
