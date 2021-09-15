3c869a6538834dc2d9faddd4061fdb241d40333b
Ghassen saaf <saafghassen@gmail.com>
2021-09-15 21:28:57 +0100

    error-handling on functions

--- controllers/hotel-controller.js
+++ controllers/hotel-controller.js
@@ -4,19 +4,31 @@ module.exports = {
     hotelsPage:function(req, res, next) {
         res.render('hotels', { title: 'Bootel | Hotels Search Results', layout :'layouts/main'});
     },
-    getHotels:async function(req, res){
-        const hotels = await hotelModel.hotelSearch(req.params.cityId, req.params.checkIn, req.params.checkOut, req.params.pax, req.params.hId);
-        const hotelsJSON = await hotels.json();
-        res.send(hotelsJSON);
+    getHotels:async function(req, res, nx){
+        try {
+            const hotels     = await hotelModel.hotelSearch(req.params.cityId, req.params.checkIn, req.params.checkOut, req.params.pax, req.params.hId);
+            const hotelsJSON = await hotels.json();
+            res.send(hotelsJSON);
+        } catch (error) {
+            res.send (error);
+        }
     },
     getFilters:async function(req, res){
-        const filters = await hotelModel.hotelFilters(req.params.searchCode);
-        const filtersJSON = await filters.json();
-        res.send(filtersJSON);
+        try {
+            const filters     = await hotelModel.hotelFilters(req.params.searchCode);
+            const filtersJSON = await filters.json();
+            res.send(filtersJSON);
+        } catch (error) {
+            res.send (error);
+        }
     },
     filter:async function(req, res){
-        const filtredRes = await hotelModel.hotelFiltredResult(req.params.page, req.params.searchCode, req.params.price, req.params.rtn, req.params.brd, req.params.thm, req.params.srt);
-        const filtredResJSON = await filtredRes.json();
-        res.send(filtredResJSON);
+        try {
+            const filtredRes     = await hotelModel.hotelFiltredResult(req.params.page, req.params.searchCode, req.params.price, req.params.rtn, req.params.brd, req.params.thm, req.params.srt);
+            const filtredResJSON = await filtredRes.json();
+            res.send(filtredResJSON);
+        } catch (error) {
+            res.send (error);
+        }
     }
 };
