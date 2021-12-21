bec2e5b03e8d92a346255884f1de4a8786a821f3
Aziz <aziz@boosterbc.com>
2021-12-21 10:43:25 +0100

    Update book-controller.js

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,8 +1,12 @@
 // import hotelModel, { hotelApiFilters } from '../models/hotel-model';
-var bookModel=require('../models/book-model');
+var bookModel = require('../models/book-model');
 module.exports = {
-    bookPage:function(req, res, next) {
-        
-        res.render('book', { title: 'Bootel | Booking',layout :'layouts/main',status : req.query.status, bookId : req.query.bookId});
+    bookPage: function (req, res, next) {
+        res.render('book', {
+            title: 'Bootel | Booking',
+            layout: 'layouts/main',
+            status: req.query.status,
+            bookId: req.query.bookId
+        });
     }
 };
